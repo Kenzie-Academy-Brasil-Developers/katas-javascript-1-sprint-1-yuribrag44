55f225527457312aa6973e8cd7839809af594e9e
Yuri <ybraga096@protonmail.com>
2021-08-10 15:12:59 -0300

    finalAtividade

--- katas1.js
+++ katas1.js
@@ -1,106 +1,91 @@
-function oneThroughTwenty() {
-  let n = 1
-  while(n<=20){console.log(n); n++} 
+function oneThroughTwenty () {
+  let result = []
+  for (let count = 1; count<=20; count++) {
+    result.push(count)
+  }
+  return result
 }
 oneThroughTwenty()
 
 
 function evensToTwenty() {
-  let count = 2
-  while (count<=20) {
-    if (count%2==0) {
-      console.log(count)
-    }
-    count ++
-    }
+  let result = []
+  for (let count = 2; count<=20; count+=2) {
+    result.push(count)
   }
-  evensToTwenty()
+  return result
+}
+evensToTwenty()
 
-  function oddsToTwenty() {
-    let count = 1
-    while (count<=20){
-      if (count%2!=0) {
-        console.log(count)
-      }
-      count ++
-    } 
-    }
-    oddsToTwenty()
-
-
-function multiplesOfFive() { 
-  let count = 5
-  while(count <= 100) {    
-    if (count%5==0) {
-      console.log(count)
-    }
-    ++count
+function oddsToTwenty() {
+  let result = []
+  for (let count = 1; count<=20; count +=2) {
+    result.push(count)
   }
-  }
-    multiplesOfFive()
+  return result
+}
+oddsToTwenty()
 
-  function squareNumbers() {
-  let number = 1 
-    let perfectSquare = number * number
-    while (number <=100) {
-      if ((perfectSquare = number * number) <= 100){
-      console.log(perfectSquare)  
-    }
-    ++ number   
-    }
-  } 
-  squareNumbers()
+function multiplesOfFive() {
+  let result = []
+  for (let count = 5; count <= 100; count+=5) {
+    result.push(count)
+  }
+  return result
+}      
+multiplesOfFive()
+
+function squareNumbers () {
+  let result = []
+  for (let number = 1; number <=10; number ++) {
+    result.push(number*number)
+  }
+  return result
+}
+squareNumbers()
     
 
-function countingBackwards(){
-  let count = 20
-  while (count >=1) {
-    console.log(count); count --
+function countingBackwards() {
+  let result = []
+  for (let count = 20; count>=1; count-- ) {
+    result.push(count)
   }
+  return result
 }
-
 countingBackwards()
 
 function evenNumbersBackwards() {
-  let count = 20
-  while (count>=2) {
-    console.log(count);count --; count --
+  let result = []
+  for (let count = 20; count>=2; count-=2) { 
+    result.push(count)
   }
-  
+  return result
  }
  evenNumbersBackwards()
 
  function oddNumbersbackwards() {
-  let count = 19
-  while (count>=1) {
-    console.log(count);count --; count --;
+  let result = []
+  for (let count = 19; count>=1; count-=2) {
+    result.push(count)
   }
-
- }
- oddNumbersbackwards()
-
- function multiplesOfFiveBackwards() {
-  let count = 100
-  while (count>=5){
-   if (count%5==0) {
-    console.log(count); 
-  }
-  count --
+  return result
 }
+oddNumbersbackwards()
+
+function multiplesOfFiveBackwards() { 
+  let result = []
+  for (let count = 100; count >= 5; count -=5) {
+    result.push(count)
+  }
+    return result
 }
 multiplesOfFiveBackwards()
 
-
 function squareNumbersBackwards() {
-  let number = 10 
-    let perfectSquare = number * number
-    while (number >= 1) {
-      if ((perfectSquare = number * number) >= 1){
-      console.log(perfectSquare)  
-    } if (perfectSquare <= 0){
-      break
-    }
-    -- number   
-    }
-  } 
-  squareNumbersBackwards()
+  let result = []
+  for (let number = 10; number >= 1; number --) {
+    result.push(number*number)
+  }
+  return result
+}
+squareNumbersBackwards()
